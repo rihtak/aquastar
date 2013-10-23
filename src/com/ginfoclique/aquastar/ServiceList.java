8235aefd1d4de3fb28a61abd627e0bcbb2df69ee
rihtak <skathiresan01@gmail.com>
2013-10-23 18:30:47 +0530

    customer,product,issues mock

---
+++ src/com/ginfoclique/aquastar/ServiceList.java
@@ -0,0 +1,15 @@
+package com.ginfoclique.aquastar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vinothrkumar on 20/10/13.
+ */
+public class ServiceList extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.service_engineer_list_row);
+    }
+}
